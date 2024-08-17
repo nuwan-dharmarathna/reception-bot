c17529bd87b43b12d9f8d1fef00b24fea3324266
nuwan-dharmarathna <dharmarathna1220@gmail.com>
2024-08-18 03:55:20 +0530

    remake order_add fuction

--- db_handle.py
+++ db_handle.py
@@ -50,15 +50,17 @@ def show_menu(meal_time:str):
     except Error as e:
         print('❌ Error occured', e)
         return "❌ An error occured while fetching the menu"
-    
 
-def check_order_item(food_items:list, meal_time:str):
+def calc_tot_price():
+    pass    
+
+def check_order_item(food_items:list, quantity:list, meal_time:str):
     try:
         # create cursor object
         cursor = connection.cursor()
         
         # Initialize result list
-        result = []
+        keys_to_delete = []
         
         # Query the database for the menu
         query = """
@@ -68,18 +70,74 @@ def check_order_item(food_items:list, meal_time:str):
             WHERE m.menu_name IN (%s, 'bakery items', 'juices') AND mi.item_name IN (%s)
         """
         
-        for item in food_items:
-            cursor.execute(query, (meal_time, item))
+        #create an order dict
+        order_dict = dict(zip(food_items, quantity))
+        
+        order_dict_with_price = {}
+        
+        for key in order_dict.keys():
+            cursor.execute(query, (meal_time, key))
             
-            result = result + cursor.fetchall()
+            result = cursor.fetchall()
             
             if not result:
-                return f"{item} is not available at the menu"
+                print(f"{key} is not available at the menu")
+                keys_to_delete.append(key)
+            else:
+                # print(f"{key}:{result[0][1]}, {result}")
+                # print(f"qty: {order_dict[key]}")
+                
+                # calculate sub tot
+                sub_tot = float(result[0][1]) * order_dict[key]
+                
+                order_dict_with_price[key] = {
+                    'quantity': order_dict[key],
+                    'item_price': float(result[0][1]),
+                    'sub_tot': sub_tot
+                }
+                
         
-        return result
-        
-        cursor.close()
+        # remove items not in menu
+        if len(keys_to_delete) > 0:
+            for key in keys_to_delete:
+                del order_dict[key]
+
+        return order_dict_with_price
             
     except Error as e:
         print('❌ Error occured', e)
         return "❌ An error occured while fetching the menu"
+    
+    finally:
+        cursor.close()
+
+def get_next_order_id():
+    try:
+        # create cursor object
+        cursor = connection.cursor()
+        
+        # Query the database for the next order id
+        query = """
+            SELECT MAX(order_id) FROM orders
+        """
+        
+        cursor.execute(query)
+        
+        result = cursor.fetchone()
+        
+        cursor.close()
+        
+        if not result:
+            return -1
+        else:
+            return result[0] + 1
+        
+    except Error as e:
+        print('❌ Error occured', e)
+        return "❌ An error occured while fetching the menu"
+
+def complete_order(order: dict):
+    # Get the next order id
+    order_id = get_next_order_id()
+    
+    
--- request_handler.py
+++ request_handler.py
@@ -18,7 +18,7 @@ def show_menu(parameters:dict, session_id:str, fullfilment_text:str):
     return JSONResponse(content={"fulfillmentText": fullfilment})
 
 def order_add(parameters:dict, session_id:str, fullfilment_text:str):
-    item_name = parameters['food_item']
+    item_names = parameters['food_item']
     quantity = parameters['number']
     
     #get current time
@@ -27,29 +27,24 @@ def order_add(parameters:dict, session_id:str, fullfilment_text:str):
     # qty as int
     quantity_as_int = [int(q) for q in quantity]
     
-    if len(item_name) != len(quantity):
+    if len(item_names) != len(quantity):
         return JSONResponse(
             content={
                 "fulfillmentText": "Please specify food items and their quantity correctly"
             }
         )
     else:
-        order_dict = dict(zip(item_name, quantity_as_int))
-        
-        #check items in the menu
-        res = db_handle.check_order_item(item_name, time)
-        checked_items = [item[0] for item in res]
-        
-        # Filtered Dict
-        filtered_order_dict = {key: value for key, value in order_dict.items() if key in checked_items}
+        #check items in the menu -> returns a dict with qty, item_price, sub_tot
+        res = db_handle.check_order_item(item_names, quantity_as_int, time)
+        # print(f"filtered_items:{res}")
         
         if session_id in inprogress_orders:
             current_order = inprogress_orders[session_id]
-            current_order.update(filtered_order_dict)
+            current_order.update(res)
             
             inprogress_orders[session_id] = current_order
         else:
-            inprogress_orders[session_id] = filtered_order_dict
+            inprogress_orders[session_id] = res
         
         fulfillment = f"{utils.format_order(inprogress_orders[session_id])}.\n " + fullfilment_text
         
@@ -96,7 +91,17 @@ def order_remove(parameters: dict, session_id: str, fullfilment_text: str):
         )
             
 def order_complete(parameters:dict, session_id:str, fullfilment_text:str):
-    pass
+    if session_id not in inprogress_orders:
+        return JSONResponse(
+            content={
+                "fulfillmentText": "No order in progress. Please try again"
+            }
+        )
+    else:
+        order = inprogress_orders[session_id]
+        
+        # save the order to the database
+        
 
 def track_order(parameters:dict, session_id:str, fullfilment_text:str):
     order_id = int(parameters['number'])
